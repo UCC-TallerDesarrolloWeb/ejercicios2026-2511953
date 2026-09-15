9e2d52c8685a65530e6647ff3ae3ffadb14ac4f4
julietafontana <2511953@ucc.edu.ar>
2026-09-14 22:28:20 -0300

    Ejercicio Hola Mundo y Conversor de Unidades

--- ejercicios2026-2511953-main/3-Js/misFunciones.js
+++ ejercicios2026-2511953-main/3-Js/misFunciones.js
@@ -5,3 +5,33 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+
+function convertirUnidad(id, valor) {
+    if (isNaN(valor) || valor === "") {
+        alert("Se ingreso un valor incorrecto: " + id);
+        document.getElementById("metro").value = "";
+        document.getElementById("pulgada").value = "";
+        document.getElementById("pie").value = "";
+        document.getElementById("yarda").value = "";
+
+    } else if (id === "metro") {
+        document.getElementById("pulgada").value = valor * 39.3701;
+        document.getElementById("pie").value = valor * 3.28084;
+        document.getElementById("yarda").value = valor * 1.09361;
+
+    } else if (id === "pulgada") {
+        document.getElementById("metro").value = valor * 0.0254;
+        document.getElementById("pie").value = valor / 12;
+        document.getElementById("yarda").value = valor / 36;
+
+    } else if (id === "pie") {
+        document.getElementById("pulgada").value = valor * 12;
+        document.getElementById("metro").value = valor * 0.3048;
+        document.getElementById("yarda").value = valor / 3;
+
+    } else if (id === "yarda") {
+        document.getElementById("pulgada").value = valor * 36;
+        document.getElementById("pie").value = valor * 3;
+        document.getElementById("metro").value = valor * 0.9144;
+    }
+}
